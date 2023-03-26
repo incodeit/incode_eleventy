8bce8b4aa22b8f10432005f6e5e962cde95d30c0
Antonio Molinari <molinari@incode.it>
2023-03-26 23:01:08 +0200

    polishing

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,9 @@ import $ from 'jquery'
 import setupSlideshow from './components/slideshow'
 import objectFitImages from 'object-fit-images'
 import LazyLoad from "vanilla-lazyload"
-import { animateElements } from './animations'
+import Typed from 'typed.js';
 
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
   const lazyLoadInstance = new LazyLoad({
     elements_selector: ".lazyload",
   })
@@ -12,5 +12,14 @@ window.onload = () => {
   const coverImages = document.querySelectorAll('.cover-image')
   objectFitImages(coverImages)
   setupSlideshow('#full-width-slideshow .slides')
-  animateElements()
-}
+    var options = {
+    strings: ["We build.", "We code.", "We design."],
+    typeSpeed: 150,
+    smartBackspace: false,
+    fadeOut: true,
+    loop: true,
+    backDelay: 2500,
+  }
+
+  if(document.getElementsByClassName("hero__typewrite")[0]) new Typed(".hero__typewrite", options);
+});
